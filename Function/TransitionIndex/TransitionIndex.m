1fe3133fda6d42e723b24dde1f4eab5abf78a93a
YongGyunKwon <ygkwon50@gmail.com>
2022-03-22 11:42:09 +0900

    Update TransitionIndex.m

--- Function/TransitionIndex/TransitionIndex.m
+++ Function/TransitionIndex/TransitionIndex.m
@@ -1,29 +1,36 @@
-%Transition 
-% (가 첫번째인 경우, 아닌경우 합친버전
+%TransitionIndex
 %REQ-0002
 
-
 %Sample Transition
-%Transition='L_ISGCtrl_ISGEnterStatus == ISG_Enable && (I_Snsr_AMBTemp <= Par_ISGCtrl_EnterAMBTempLow || I_Snsr_AMBTemp >= Par_ISGCtrl_EnterAMBTempHgh)';
-
 Transition = '(AA || (BB && (CC || DD) || (EE && FF)) || (GG || HH))';
-%Transition = 'AA||(BB&&CC||(DD))';
-
+%결과값 보기
 disp(Transition_Index(Transition));
 
-%Transition 
+% Transition Index
+% 해당 함수를 사용하기 위해 아래에 선언된 함수들이 모두 존재해야함.
+% 사용함수
+% Transition_Index_InStart : 괄호가 '('로 시작되는 경우
+% Transition_Index_InNotStart : 괄호로 Transition이 시작되지 않는 경우
+% tab_counter : 괄호에 따라 tab을 몇번 해야 하는지 계산
+% 매개변수 Transition에는 Transition식을 넣어야 함 (UI에서 Transition 자리값 입력받아야함)
+% result_print로 결과값 리턴(UI에서 결과값을 넣기위한 변수는 Transition_Index(Transition) 라고 보면됨)
 function result_print=Transition_Index(Transition)
 
     Transition_char=char(Transition);
-    
+    % '('로 시작될 경우
+    % function Transition_Index_Start()을 사용
     if Transition_char(1)=='('
         result_print=Transition_Index_InStart(Transition);
+
+    % '('로 시작되지 않을 경우
+    %Transition_Index_InNotStart()을 사용
     else
         result_print=Transition_Index_InNotStart(Transition);
     end
 
 end
 
+%'('로 시작하는 Transition
 function result_print_InStart=Transition_Index_InStart(Transition)
 
     Transition_size=size(Transition,2);
@@ -44,7 +51,12 @@ function result_print_InStart=Transition_Index_InStart(Transition)
     for i=1:Transition_size
 
         now_char=Transition_char(i);
-
+        next_char='';
+        
+        if i<Transition_size
+            next_char=Transition_char(i+1);
+        end
+        
         % ( 부분
         % Use bracket_count_index_in
         if now_char=='('
@@ -70,6 +82,7 @@ function result_print_InStart=Transition_Index_InStart(Transition)
         % Use bracket_count_index_out
         elseif now_char==')'
             %첫번째 ) 일 경우
+                
             if bracket_count_index_out==bracket_count
                 result=append(result,now_char);
 
@@ -78,6 +91,12 @@ function result_print_InStart=Transition_Index_InStart(Transition)
             elseif bracket_count_index_out==1
                 result=append(result,'\n');
                 result=append(result,now_char);
+            %다음글자가 ')' 일 경우
+            elseif next_char==')'
+                result=append(result,now_char);
+                
+                bracket_count_index_out=bracket_count_index_out-1;
+    
             %그 외의 )
             else
                 result=append(result,'\n');
@@ -100,11 +119,13 @@ function result_print_InStart=Transition_Index_InStart(Transition)
 
 end
 
+%'('로 시작하지 않은 Transition
 function result_print_InNotStart=Transition_Index_InNotStart(Transition)
     Transition_size=size(Transition,2);
 
     Transition_char=char(Transition);
-
+    
+    
     %괄호갯수 count
     bracket_count=count(Transition,'(');
 
@@ -119,7 +140,13 @@ function result_print_InNotStart=Transition_Index_InNotStart(Transition)
     for j=1:Transition_size
 
         now_char=Transition_char(j);
-
+        
+        next_char='';
+        
+        if j<Transition_size
+            next_char=Transition_char(j+1);
+        end
+        
         % ( 부분
         % Use bracket_count_index_in
         if now_char=='('
@@ -140,7 +167,11 @@ function result_print_InNotStart=Transition_Index_InNotStart(Transition)
                 result=append(result,now_char);
 
                 bracket_count_index_out=bracket_count_index_out-1;
-
+            %다음글자가 ')' 일 경우
+            elseif next_char==')'
+                result=append(result,now_char);
+                
+                bracket_count_index_out=bracket_count_index_out-1;
             %그 외의 )
             else
                 result=append(result,'\n');
@@ -165,5 +196,6 @@ function taab=tab_counter(tab_number)
     taab='';
     for tab_i=1:(tab_number)
         taab=append(taab,'\t');
+        
     end
 end
